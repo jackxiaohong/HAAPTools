813c243664af1efabaa3761d007b1a65695d9420
sss <loxoll_matthew@126.com>
2018-10-20 14:25:04 +0800

    SW part of main Test OK, need Add more ...

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import ClassSW as sw
 import ClassHAAP as haap
 import Source as s
 from collections import OrderedDict as Odd
-import configparser as cp
 import os
 import sys
 import datetime
 import getpass
 import pprint
+
+try:
+    import configparser as cp
+except Exception:
+    import ConfigParser as cp
 
 #----------------------------------------- 动态对象实例化及访问
 # lstSW = ['172.16.254.75', '172.16.254.76']
@@ -64,24 +68,31 @@ objCFG.read('Conf.ini')
 
 # <<<SAN Switch Config>>>
 strSWUser = objCFG.get('SWSetting', 'username')
-strSWPWD = objCFG.get('SWSetting', 'password')
 intSWSSHPort = objCFG.get('SWSetting', 'port')
 
 oddSWPort = Odd()
 for i in objCFG.items('SWPorts'):
     oddSWPort[i[0]] = eval(i[1])
-
 lstSW = list(oddSWPort.keys())
 lstSWPorts = list(oddSWPort.values())
 
-strSWPortErrorFolder = objCFG.get('FolderSetting', 'porterrfolder')
+strSWPWD = objCFG.get('SWSetting', 'password')
+if strSWPWD:
+    strSWPWD = strSWPWD
+else:
+    strSWPWD = getpass.getpass(
+        prompt='Please Input Your SAN Switch Password for User {}:'.format(
+            strSWUser), stream=None)
+
+
+strSWPortErrorFolder = objCFG.get('FolderSetting', 'swporterr')
 
 # <<<HAAP Config>>>
-lstEngineIPs = list(i[0] for i in objCFG.items('Engines'))
-intTraceLevel = int(objCFG.get('TraceSetting', 'TraceLevel'))
-intTelnetPort = int(objCFG.get('EngineSetting', 'TelnetPort'))
+lstHAAP = list(i[0] for i in objCFG.items('Engines'))
+intTLevel = int(objCFG.get('TraceSetting', 'TraceLevel'))
+intTNPort = int(objCFG.get('EngineSetting', 'TelnetPort'))
 intFTPPort = int(objCFG.get('EngineSetting', 'FTPPort'))
-lstCheckCommand = list(i[0] for i in objCFG.items('PeriodicCheckCommand'))
+lstCheckCMD = list(i[0] for i in objCFG.items('PeriodicCheckCommand'))
 
 strHAAPPasswd = objCFG.get('EngineSetting', 'HAAPPassword')
 if strHAAPPasswd:
@@ -90,9 +101,36 @@ else:
     strHAAPPasswd = getpass.getpass(
         prompt='Please Input Your Engine Password: ', stream=None)
 
+#TCFolder = HAAP Trace Folder
+strTCFolder = objCFG.get('FolderSetting', 'trace')
+#CFGFolder = HAAP Config Backup Folder
+strCFGFolder = objCFG.get('FolderSetting', 'cfgbackup')
+#PCFolder = HAAP Periodic Check Result Folder
+strPCFolder = objCFG.get('FolderSetting', 'PeriodicCheck')
+
+def _get_SWInstance():
+    oddSWInst = Odd()
+    for i in range(len(lstSW)):
+        oddSWInst[lstSW[i]] = sw.SANSW(lstSW[i], intSWSSHPort,
+                                       strSWUser, strSWPWD, lstSWPorts[i])
+    return oddSWInst
+
+def _get_HAAP_TNInstance():
+    oddTNInst = Odd()
+    for i in range(len(lstEngineIPs)):
+        oddSWInst[lstSW[i]] = sw.SANSW(lstSW[i], intSWSSHPort,
+                                       strSWUser, strSWPWD, lstSWPorts[i])
+        HAAP('172.16.254.71', 23, '.com', 21)
+    return oddSWInst
+
+def _get_HAAP_FTPInstance():
+    oddSWInst = Odd()
+    for i in range(len(lstSW)):
+        oddSWInst[lstSW[i]] = sw.SANSW(lstSW[i], intSWSSHPort,
+                                       strSWUser, strSWPWD, lstSWPorts[i])
+    return oddSWInst
 
 def main():
-
     if len(sys.argv) == 1 or len(sys.argv) > 2:
         print(strHelp)
 
@@ -104,14 +142,12 @@ def main():
     #     moduleZipCollections.ZipCollections()
 
     elif sys.argv[1] == '-porterrshow':
-        oddSWInst = Odd()
-        for i in range(len(lstSW)):
-            oddSWInst[lstSW[i]] = sw.SANSW(lstSW[i], intSWSSHPort,
-                                           strSWUser, strSWPWD, lstSWPorts[i])
-        print(oddSWInst)
         for i in lstSW:
-            oddSWInst[i].show_porterrors()
+            _get_SWInstance()[i].show_porterrors()
 
+    elif sys.argv[1] == '-clearporterrorAll':
+        for i in lstSW:
+            _get_SWInstance()[i].clear_porterr_All()
     # elif sys.argv[1] == '-statsclear':
     #     moduleClearPortError.ClearPortError()
 
